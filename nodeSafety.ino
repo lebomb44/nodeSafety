06eac67a1694f9edd43c01438240c063327a0686
LeBomb <cambon.olivier@gmail.com>
2019-02-10 20:28:49 +0100

    Add entry

--- nodeSafety.ino
+++ nodeSafety.ino
@@ -15,10 +15,10 @@ const char moveDiningContactName[] PROGMEM = "moveDiningContact";
 const char moveDiningNotUsedContactName[] PROGMEM = "moveDiningNotUsedContact";
 const char moveEntryContactName[] PROGMEM = "moveEntryContact";
 const char moveEntryNotUsedContactName[] PROGMEM = "moveEntryNotUsedContact";
-const char moveSpareContactName[] PROGMEM = "moveSpareContact";
-const char moveSpareNotUsedContactName[] PROGMEM = "moveSpareNotUsedContact";
+const char doorEntryContactName[] PROGMEM = "doorEntryContact";
+const char lightAlarmName[] PROGMEM = "lightAlarm";
 const char moveRelayName[] PROGMEM = "moveRelay";
-const char out0RelayName[] PROGMEM = "out0Relay";
+const char buzzerRelayName[] PROGMEM = "buzzerRelay";
 const char out1RelayName[] PROGMEM = "out1Relay";
 const char out2RelayName[] PROGMEM = "out2Relay";
 const char out3RelayName[] PROGMEM = "out3Relay";
@@ -30,10 +30,10 @@ Contact moveDiningContact(moveDiningContactName, 10);
 Contact moveDiningNotUsedContact(moveDiningNotUsedContactName, 9);
 Contact moveEntryContact(moveEntryContactName, A1);
 Contact moveEntryNotUsedContact(moveEntryNotUsedContactName, A0);
-Contact moveSpareContact(moveSpareContactName, A3);
-Contact moveSpareNotUsedContact(moveSpareNotUsedContactName, A2);
+Contact doorEntryContact(doorEntryContactName, A2);
+Relay lightAlarm(lightAlarmName, A3);
 Relay moveRelay(moveRelayName, 13);
-Relay out0Relay(out0RelayName, 3);
+Relay buzzerRelay(buzzerRelayName, 3);
 Relay out1Relay(out1RelayName, 4);
 Relay out2Relay(out2RelayName, 5);
 Relay out3Relay(out3RelayName, 6);
@@ -45,10 +45,13 @@ void ping_cmdGet(int arg_cnt, char **args) { cnc_print_cmdGet_u32(pingName, loop
 void moveCoridorContact_cmdGet(int arg_cnt, char **args) { moveCoridorContact.cmdGet(arg_cnt, args); }
 void moveDiningContact_cmdGet(int arg_cnt, char **args) { moveDiningContact.cmdGet(arg_cnt, args); }
 void moveEntryContact_cmdGet(int arg_cnt, char **args) { moveEntryContact.cmdGet(arg_cnt, args); }
+void doorEntryContact_cmdGet(int arg_cnt, char **args) { doorEntryContact.cmdGet(arg_cnt, args); }
+void lightAlarm_cmdGet(int arg_cnt, char **args) { lightAlarm.cmdGet(arg_cnt, args); }
+void lightAlarm_cmdSet(int arg_cnt, char **args) { lightAlarm.cmdSet(arg_cnt, args); }
 void moveRelay_cmdGet(int arg_cnt, char **args) { moveRelay.cmdGet(arg_cnt, args); }
 void moveRelay_cmdSet(int arg_cnt, char **args) { moveRelay.cmdSet(arg_cnt, args); }
-void out0Relay_cmdGet(int arg_cnt, char **args) { out0Relay.cmdGet(arg_cnt, args); }
-void out0Relay_cmdSet(int arg_cnt, char **args) { out0Relay.cmdSet(arg_cnt, args); }
+void buzzerRelay_cmdGet(int arg_cnt, char **args) { buzzerRelay.cmdGet(arg_cnt, args); }
+void buzzerRelay_cmdSet(int arg_cnt, char **args) { buzzerRelay.cmdSet(arg_cnt, args); }
 void out1Relay_cmdGet(int arg_cnt, char **args) { out1Relay.cmdGet(arg_cnt, args); }
 void out1Relay_cmdSet(int arg_cnt, char **args) { out1Relay.cmdSet(arg_cnt, args); }
 void out2Relay_cmdGet(int arg_cnt, char **args) { out2Relay.cmdGet(arg_cnt, args); }
@@ -69,20 +72,24 @@ void setup() {
   cnc_cmdGet_Add(moveCoridorContactName , moveCoridorContact_cmdGet);
   cnc_cmdGet_Add(moveDiningContactName, moveDiningContact_cmdGet);
   cnc_cmdGet_Add(moveEntryContactName, moveEntryContact_cmdGet);
-  cnc_cmdSet_Add(moveRelayName, moveRelay_cmdGet);
+  cnc_cmdGet_Add(doorEntryContactName, doorEntryContact_cmdGet);
+  cnc_cmdGet_Add(lightAlarmName, lightAlarm_cmdGet);
+  cnc_cmdSet_Add(lightAlarmName, lightAlarm_cmdSet);
+  cnc_cmdGet_Add(moveRelayName, moveRelay_cmdGet);
   cnc_cmdSet_Add(moveRelayName, moveRelay_cmdSet);
-  cnc_cmdSet_Add(out0RelayName, out0Relay_cmdGet);
-  cnc_cmdSet_Add(out0RelayName, out0Relay_cmdSet);
-  cnc_cmdSet_Add(out1RelayName, out1Relay_cmdGet);
+  cnc_cmdGet_Add(buzzerRelayName, buzzerRelay_cmdGet);
+  cnc_cmdSet_Add(buzzerRelayName, buzzerRelay_cmdSet);
+  cnc_cmdGet_Add(out1RelayName, out1Relay_cmdGet);
   cnc_cmdSet_Add(out1RelayName, out1Relay_cmdSet);
-  cnc_cmdSet_Add(out2RelayName, out2Relay_cmdGet);
+  cnc_cmdGet_Add(out2RelayName, out2Relay_cmdGet);
   cnc_cmdSet_Add(out2RelayName, out2Relay_cmdSet);
-  cnc_cmdSet_Add(out3RelayName, out3Relay_cmdGet);
+  cnc_cmdGet_Add(out3RelayName, out3Relay_cmdGet);
   cnc_cmdSet_Add(out3RelayName, out3Relay_cmdSet);
-  cnc_cmdSet_Add(out4RelayName, out4Relay_cmdGet);
+  cnc_cmdGet_Add(out4RelayName, out4Relay_cmdGet);
   cnc_cmdSet_Add(out4RelayName, out4Relay_cmdSet);
+  lightAlarm.open();
   moveRelay.open();
-  out0Relay.open();
+  buzzerRelay.open();
   out1Relay.open();
   out2Relay.open();
   out3Relay.open();
@@ -91,8 +98,9 @@ void setup() {
 
 void loop() {
   delay(1);
+  lightAlarm.run(false);
   moveRelay.run(false);
-  out0Relay.run(false);
+  buzzerRelay.run(false);
   out1Relay.run(false);
   out2Relay.run(false);
   out3Relay.run(false);
@@ -103,9 +111,9 @@ void loop() {
     moveCoridorContact.run(true);
     moveDiningContact.run(true);
     moveEntryContact.run(true);
+    doorEntryContact.run(true);
   }
   cncPoll();
   loopNb++;
   if(1000000000 <= loopNb) { loopNb = 0; }
 }
-
